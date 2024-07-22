8260afb62ae56c814f47b46393d8906d279b44ea
Kitagawa19 <kkitagawa16219@gmail.com>
2024-07-22 17:41:09 +0900

    add test code

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+    "database/sql"
+
+    _ "github.com/mattn/go-sqlite3"
+)
+
+var DbConnection *sql.DB
+
+func main() {
+    DbConnection, _ := sql.Open("sqlite3", "new.db") //接続開始（example.sqlに保存する）
+    defer DbConnection.Close()                              //最後は確実にクローズする。
+
+    //この下に、CREATE文　SELECT文　INSERT文 UPDATE文　DELETE文を記載する
+
+}
