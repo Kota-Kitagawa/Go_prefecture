52d7d327b249ae030b5fc46cccc1e4f565b8bf6b
Kitagawa19 <kkitagawa16219@gmail.com>
2024-07-22 17:54:43 +0900

    add test code

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
-    "database/sql"
+	"database/sql"
+	"fmt"
+	"log"
 
-    _ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var DbConnection *sql.DB
 
 func main() {
-    DbConnection, _ := sql.Open("sqlite3", "new.db") //接続開始（example.sqlに保存する）
-    defer DbConnection.Close()                              //最後は確実にクローズする。
+	var err error
+	if err != nil {
+		log.Fatal(err)
+	}
+	DbConnection, _ := sql.Open("sqlite3", "new.db") //接続開始（example.sqlに保存する）
+	defer DbConnection.Close()                       //最後は確実にクローズする。
 
-    //この下に、CREATE文　SELECT文　INSERT文 UPDATE文　DELETE文を記載する
+	//この下に、CREATE文　SELECT文　INSERT文 UPDATE文　DELETE文を記載する
+	readUserSQL := `SELECT *  FROM  utf_ken_all LIMIT 5`
+	rows, err := DbConnection.Query(readUserSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
 
 }
