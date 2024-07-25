bc7b19d2a683048c5fb2988d5b436ffd1a64d9e2
Kitagawa19 <kkitagawa16219@gmail.com>
2024-07-26 02:26:39 +0900

    add  search postalcode handlers

--- handlers/address.go
+++ handlers/address.go
@@ -2,5 +2,22 @@ package handlers
 
 import (
 	"net/http"
-	""
+	"GO_prefecture/models"
+	"github.com/gin-gonic/gin"
 )
+
+func IndexHandler(c *gin.Context) {
+	c.HTML(http.StatusOK,"index.html",gin.H{})
+}
+func SearchHandler(c *gin.Context) {
+	prefecture := c.PostForm("prefecture")
+	city := c.PostForm("city")
+	town := c.PostForm("town")
+	street := c.PostForm("street")
+	postalCode,err := models.GetPostalCode(prefecture,city,town,street)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError,gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK.gin.H("postal_code":postalCode))
+}
