10230d578dab8255f0f7f4b8fca3c8be0281a2d6
Kitagawa19 <kkitagawa16219@gmail.com>
2024-11-18 17:24:34 +0900

    fix 二重で正規表現を行っていたため修正

--- handlers/prefectures.go
+++ handlers/prefectures.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"log"
 	"net/http"
-	"regexp"
 	"github.com/gin-gonic/gin"
 	"Go_prefecture/database"
 )
@@ -15,17 +14,16 @@ func PrefectureHandler(c *gin.Context) {
 		return
 	}
 	defer rows.Close()
-	// 正規表現のコードを二回使っている上のSELEｃT分と下のMuxtCompileの文
-
+	
 	var prefectures []string
-	reader:=regexp.MustCompile(`^[\p{Han}]{2,3}(?:都|道|府|県)$`)
-
 	for rows.Next() {
 		var prefecture string
-		rows.Scan(&prefecture)
-		if(reader.MatchString(prefecture)){
-			prefectures = append(prefectures, prefecture)
+		if err:=rows.Scan(&prefecture); err != nil {
+			log.Printf("Failed to scan prefecture: %v", err)
+			c.String(http.StatusInternalServerError, "Failed to scan prefecture")
+			return
 		}
+		prefectures = append(prefectures, prefecture)
 	}
 	c.HTML(http.StatusOK, "prefectures.html", gin.H{
 		"Prefectures": prefectures,
