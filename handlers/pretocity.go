b8a76294b5b37ad06ecbe132854fffb43472c5d0
Kitagawa19 <kkitagawa16219@gmail.com>
2024-12-02 18:02:26 +0900

    update コードの関数化とJSON形式の出力の追加(#8)

--- handlers/pretocity.go
+++ handlers/pretocity.go
@@ -7,26 +7,45 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func PrefecturetocityHandler(c *gin.Context) {
-	rows, err := database.DB.Query("SELECT DISTINCT field7 FROM addresses")
-	if err != nil {
-		log.Printf("Failed to fetch prefectures: %v", err)
-		c.String(http.StatusInternalServerError, "Failed to fetch prefectures")
-		return
-	}
-	defer rows.Close()
+func fetchPretoCity()([]string,error){
+	query := `SELECT DISTINCT field7 FROM addresses`
+    rows, err := database.DB.Query(query)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+    var prefectures []string
+    for rows.Next() {
+        var prefecture string
+        if err := rows.Scan(&prefecture); err != nil {
+            return nil, err
+        }
+        prefectures = append(prefectures, prefecture)
+    }
+    return prefectures, nil
+}
 
-	var prefectures []string
-	for rows.Next() {
-		var prefecture string
-		if err:=rows.Scan(&prefecture); err != nil {
-			log.Printf("Failed to scan prefecture: %v", err)
-			c.String(http.StatusInternalServerError, "Failed to scan prefecture")
-			return
-		}
-		prefectures = append(prefectures, prefecture)
-	}
-	c.HTML(http.StatusOK, "cities.html", gin.H{
-		"Prefectures": prefectures,
-	})
+func PretoCityHTMLHandler(c *gin.Context) {
+    prefectures, err := fetchPrefecture()
+    if err != nil {
+        log.Printf("Failed to fetch prefectures: %v", err)
+        c.String(http.StatusInternalServerError, "Failed to fetch prefectures")
+        return
+    }
+    c.HTML(http.StatusOK, "cities.html", gin.H{
+        "Prefectures": prefectures,
+    })
+}
+
+func PretoCityJSONHandler(c *gin.Context) {
+    prefectures, err := fetchPrefecture()
+    if err != nil {
+        log.Printf("Failed to fetch prefectures: %v", err)
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch prefectures"})
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{
+        "Prefectures": prefectures,
+    })
 }
