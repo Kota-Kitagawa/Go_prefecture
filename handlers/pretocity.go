132e85784dbb89ad07f19f7f805ac7f53d73d176
KitagawaKouta <kkitagawa16219@gmail.com>
2024-12-27 00:42:24 +0900

    clean facadeとfactory methodを使用したコードに変更(#14)

--- handlers/pretocity.go
+++ handlers/pretocity.go
@@ -3,46 +3,21 @@ package handlers
 import (
     "log"
     "net/http"
-    "Go_prefecture/internal/database"
+    "Go_prefecture/pkg"
     "github.com/gin-gonic/gin"
 )
 
-func fetchPretoCity()([]string,error){
-    query := `SELECT DISTINCT field7 FROM addresses`
-    rows, err := database.DB.Query(query)
-    if err != nil {
-        return nil, err
-    }
-    defer rows.Close()
-    var prefectures []string
-    for rows.Next() {
-        var prefecture string
-        if err := rows.Scan(&prefecture); err != nil {
-            return nil, err
-        }
-        prefectures = append(prefectures, prefecture)
-    }
-    return prefectures, nil
-}
-
-func PretoCityHTMLHandler(c *gin.Context) {
-    prefectures, err := fetchPretoCity()
+func PretoCityHandler(c *gin.Context) {
+    prefectures, err := pkg.FetchPretoCity()
     if err != nil {
         log.Printf("Failed to fetch prefectures: %v", err)
         c.String(http.StatusInternalServerError, "Failed to fetch prefectures")
         return
     }
-    c.HTML(http.StatusOK, "prefectures.html", gin.H{
+    responseFormat := c.DefaultQuery("format","html")
+    res :=pkg.GetResponse(responseFormat,"prefectures.html")
+    res.Respond(c,gin.H{
         "Prefectures": prefectures,
     })
 }
 
-func PretoCityJSONHandler(c *gin.Context) {
-    prefectures, err := fetchPretoCity()
-    if err != nil {
-        log.Printf("Failed to fetch prefectures: %v", err)
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch prefectures"})
-        return
-    }
-    c.JSON(http.StatusOK, gin.H{"Prefectures": prefectures})
-}
