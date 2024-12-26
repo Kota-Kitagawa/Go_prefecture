7954292bb8df7792f7d5196fbaf693871df4adb7
KitagawaKouta <kkitagawa16219@gmail.com>
2024-12-27 00:09:14 +0900

    add 県一覧を取得するハンドラーの作成(#14)

---
+++ handlers/prefecture_list.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+    "log"
+    "net/http"
+    "github.com/gin-gonic/gin"
+    "Go_prefecture/pkg"
+)
+
+func PrefListHTMLHandler(c *gin.Context) {
+    prefectures, err := pkg.FetchPrefecture()
+    if err != nil {
+        log.Printf("Failed to fetch prefectures: %v", err)
+        c.String(http.StatusInternalServerError, "Failed to fetch prefectures")
+        return
+    }
+	responseFormat := "html"
+    res :=pkg.GetResponse(responseFormat,"cities.html")
+    res.Respond(c,gin.H{
+        "Prefectures": prefectures,
+    })
+}
