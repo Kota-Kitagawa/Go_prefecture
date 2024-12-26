0868cfc156dff005738fb3164210676ad6090623
KitagawaKouta <kkitagawa16219@gmail.com>
2024-12-27 00:41:44 +0900

    fix 不必要になったエンドポイントの削除(#14)

--- cmd/main.go
+++ cmd/main.go
@@ -70,8 +70,7 @@ func main() {
     rows.Close()
 
     router.GET("/", handlers.HomeHandler)
-    router.GET("/prefectures", handlers.PretoCityHTMLHandler) //都道府県リストページ
-    router.GET("/api/prefectures", handlers.PretoCityJSONHandler)
+    router.GET("/prefectures", handlers.PretoCityHandler) //都道府県リストページ
     router.GET("/cities", handlers.PrefListHTMLHandler)   //市区町村検索ページ
     router.GET("/citiesresult", handlers.CitiesHandler)        //市区町村検索結果のページ
     router.GET("/postcode", handlers.PostalHandler)           //郵便番号の検索ページ
