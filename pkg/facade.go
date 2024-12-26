f0ca1442f58d5949126fe1657fde44c055be2490
KitagawaKouta <kkitagawa16219@gmail.com>
2024-12-26 14:58:06 +0900

    add facadeの追加(#14)

--- pkg/facade.go
+++ pkg/facade.go
@@ -22,3 +22,29 @@ func FetchAddress(postalCode string) (string, string, string, string, error) {
 
     return field7, field8, field9, fullAddress, nil
 }
+
+func FetchCities(prefecture string, limit, offset int) ([]string, error) {
+    query := `
+        SELECT CASE
+            WHEN field9 = '以下に掲載がない場合' THEN field8
+            ELSE field8 || field9
+            END AS city
+        FROM addresses
+        WHERE field7 = ?
+        LIMIT ? OFFSET ?
+    `
+    rows, err := database.DB.Query(query, prefecture, limit, offset)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+    var cities []string
+    for rows.Next() {
+        var city string
+        if err := rows.Scan(&city); err != nil {
+            return nil, err
+        }
+        cities = append(cities, city)
+    }
+    return cities, err
+}
